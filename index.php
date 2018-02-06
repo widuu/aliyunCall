37f54a073b8d34f4940b0e68a81f223909fe8989
京际科技 <developer@ttchina.com>
2018-02-06 15:32:32 +0800

    delete

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require "src/Auth.php";
-$a = new widuu\aliyunCall\Auth(['client_id'=>'920171048','client_secret'=>'IOnron7qCh86uxxuMDViDdVgCerxMNb9YNMM7NZFQSTdtBCIaTsrMjidMQx8VK2l','redirect_uri'=>'https://data.zhongze.cc/auth/callback']);
-//$a->login();
